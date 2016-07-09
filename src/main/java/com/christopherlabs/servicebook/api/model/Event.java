d8c681de34d811331f6f8c4415ac5db7ac807c6e
willmsn98 <cwilliamson@Christopher-Williamsons-MacBook-Pro-3.local>
2016-07-08 22:43:51 -0500

    Added description field to event

--- src/main/java/com/christopherlabs/servicebook/api/model/Event.java
+++ src/main/java/com/christopherlabs/servicebook/api/model/Event.java
@@ -18,6 +18,7 @@ import java.util.Collection;
 public class Event {
     private long id;
     private String name;
+    private String description;
     private Organization organization;
     private String address;
     private String city;
@@ -46,6 +47,14 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @ManyToOne
